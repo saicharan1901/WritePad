99891c41b1e79a403a9969e0ba51a3946c6bd77b
saicharan1901 <papinenisaicharan@gmail.com>
2023-04-11 20:08:40 +0530

    Update [...nextauth].js

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,13 +1,14 @@
 import NextAuth from 'next-auth'
-import GitHubProvider from "next-auth/providers/github";
+import GitHubProvider from 'next-auth/providers/github'
 
 const options = {
-    providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_CLIENT_ID,
-            clientSecret: process.env.GITHUB_CLIENT_SECRET,
-        }),
-    ],
+  providers: [
+    GitHubProvider({
+      clientId: process.env.GITHUB_CLIENT_ID,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET,
+    }),
+  ],
+  secret: process.env.SECRET,
 }
 
 export default (req, res) => NextAuth(req, res, options)
