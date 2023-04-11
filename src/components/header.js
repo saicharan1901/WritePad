178de0a309d0a71e9994c4e3bfae70390db2183a
saicharan1901 <papinenisaicharan@gmail.com>
2023-04-11 20:02:59 +0530

    update

--- src/components/header.js
+++ src/components/header.js
@@ -1,43 +1,95 @@
-import React from "react";
+import React, { useState } from "react";
 import Link from "next/link";
-// import { useSession, signIn, signOut } from 'next-auth/react'
-
+import { useSession, signIn, signOut } from 'next-auth/react'
 
 export default function Header() {
-    // const { data: session } = useSession()
+  const { data: session } = useSession()
 
-    // const handleSignin = (e) => {
-    //   e.preventDefault()
-    //   signIn()
-    // }
-  
-    // const handleSignout = (e) => {
-    //   e.preventDefault()
-    //   signOut()
-    // }
-    
+  const handleSignin = (e) => {
+    e.preventDefault()
+    signIn()
+  }
+
+  const handleSignout = (e) => {
+    e.preventDefault()
+    signOut()
+  }
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const handleMenuToggle = () => {
+        setMenuOpen(!menuOpen);
+    };
+
     return (
         <nav className="bg-[#202124] shadow-lg">
-            <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 font-mono">
-                <div className="relative flex items-center justify-between h-16">
-                    <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
-                        <div className="flex-shrink-0">
-                            <Link href='/home' passHref>
-                                <div className=" text-3xl font-bold text-yellow-700">
-                                    <svg width="32" height="32" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                                        <path d="M10 0h44c5.523 0 10 4.477 10 10v44c0 5.523-4.477 10-10 10H10C4.477 64 0 59.523 0 54V10C0 4.477 4.477 0 10 0zm1 1c-4.411 0-8 3.589-8 8v44c0 4.411 3.589 8 8 8h44c4.411 0 8-3.589 8-8V10c0-4.411-3.589-8-8-8H11z" fill="#F9E07F" />
-                                        <rect x="14" y="14" width="36" height="36" fill="none" stroke="#5C5C5C" stroke-width="2" />
-                                        <line x1="14" y1="23" x2="50" y2="23" stroke="#5C5C5C" stroke-width="2" />
-                                        <line x1="14" y1="29" x2="50" y2="29" stroke="#5C5C5C" stroke-width="2" />
-                                        <line x1="14" y1="35" x2="50" y2="35" stroke="#5C5C5C" stroke-width="2" />
-                                    </svg>
-                                </div>
-                            </Link>
-
-                        </div>
-                    </div>
-
-                    {/* {session &&
+            <div className="hidden sm:block sm:ml-6">
+                <div className="flex space-x-4">
+                    <Link href="/home" passHref>
+                        <p className="text-white hover:bg-yellow-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                            Home
+                        </p>
+                    </Link>
+                    <Link href="/note" passHref>
+                        <p className="text-white hover:bg-yellow-600 hover:text-white px-3 py-2 rounded-md text-sm font-medium">
+                            Notes
+                        </p>
+                    </Link>
+                </div>
+            </div>
+            {/* Mobile menu button */}
+            <div className="-mr-2 flex sm:hidden">
+                <button
+                    type="button"
+                    className="bg-[#FBBF24] inline-flex items-center justify-center p-2 rounded-md text-gray-900 hover:text-white hover:bg-yellow-600 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                    aria-expanded={menuOpen}
+                    onClick={handleMenuToggle}
+                >
+                    <span className="sr-only">Open main menu</span>
+                    <svg
+                        className="block h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M4 6h16M4 12h16M4 18h16"
+                        />
+                    </svg>
+                    <svg
+                        className="hidden h-6 w-6"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                        aria-hidden="true"
+                    >
+                        <path
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth={2}
+                            d="M6 18L18 6M6 6l12 12"
+                        />
+                    </svg>
+                </button>
+            </div>
+            {/* Mobile menu */}
+            <div
+                className={`${menuOpen ? "block" : "hidden"
+                    } sm:hidden bg-[#202124] pb-3`}
+            >
+                <Link href="/home" passHref>
+                    <p className="block px-3 py-2 text-white font-medium">Home</p>
+                </Link>
+                <Link href="/note" passHref>
+                    <p className="block px-3 py-2 text-white font-medium">Notes</p>
+                </Link>
+            </div>
+            {session &&
             <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
               <div className="flex-shrink-0">
                 <img className="h-10 w-10 rounded-full" src={session.user.image} alt={session.user.name} />
@@ -46,39 +98,25 @@ export default function Header() {
                 <div className="text-white font-medium">{session.user.name}</div>
               </div>
             </div>
-          } */}
-
-                <div className="hidden sm:block sm:ml-6">
-                        <div className="flex space-x-4">
-                            <Link href='/note' passHref>
-                                <div className=" hover:bg-yellow-700 font-mono px-3 py-2  rounded-lg text-yellow-700 hover:text-[#202124]">Notes</div>
-                            </Link>
-                        </div>
-                    </div>
-
-          {/* {session &&
+          }
+            {session &&
             <div className="hidden sm:block sm:ml-6">
               <div className="flex space-x-4">
-                <button className=" hover:bg-yellow-700 font-mono px-3 py-2  rounded-lg text-yellow-700 hover:text-[#202124] " onClick={handleSignout}>
+                <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" onClick={handleSignout}>
                   Sign out
                 </button>
               </div>
             </div>
-          } */}
-          {/* {!session &&
+          }
+          {!session &&
             <div className="hidden sm:block sm:ml-6">
               <div className="flex space-x-4">
-                <button className=" hover:bg-yellow-700 font-mono px-3 py-2  rounded-lg text-yellow-700 hover:text-[#202124]" onClick={handleSignin}>
+                <button className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium" onClick={handleSignin}>
                   Sign in
                 </button>
               </div>
             </div>
-          } */}
-
-
-                </div>
-            </div>
+          }
         </nav>
-
     );
 }
--- src/pages/home.js
+++ src/pages/home.js
@@ -5,58 +5,48 @@ export default function HomePage() {
     return (
         <div className="min-h-screen bg-[#202124] font-mono">
 
-
             <Header />
 
-
             <div className="flex flex-col items-center justify-center mt-12">
-                <h1 className="text-4xl font-bold text-center text-yellow-500 mb-8 ">
-                    <div className="w-12 h-12 absolute animate-bounce ">
-                        <svg width="64" height="64" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
-                            <path d="M10 0h44c5.523 0 10 4.477 10 10v44c0 5.523-4.477 10-10 10H10C4.477 64 0 59.523 0 54V10C0 4.477 4.477 0 10 0zm1 1c-4.411 0-8 3.589-8 8v44c0 4.411 3.589 8 8 8h44c4.411 0 8-3.589 8-8V10c0-4.411-3.589-8-8-8H11z" fill="#F9E07F" />
-                            <rect x="14" y="14" width="36" height="36" fill="none" stroke="#5C5C5C" strokeWidth="3" />
-                            <line x1="14" y1="23" x2="50" y2="23" stroke="#5C5C5C" strokeWidth="2" />
-                            <line x1="14" y1="29" x2="50" y2="29" stroke="#5C5C5C" strokeWidth="2" />
-                            <line x1="14" y1="35" x2="50" y2="35" stroke="#5C5C5C" strokeWidth="2" />
-                        </svg>
+                <h1 className="text-2xl sm:text-4xl font-bold text-center text-yellow-500 mb-8 ">
+                    <div className="w-8 h-8 sm:w-12 sm:h-12 absolute animate-bounce ">
                     </div>
-                    <div className="ml-24">Simplify your life with WritePad!</div>
+                    <div className="ml-2 sm:ml-24">Simplify your life with WritePad!</div>
                 </h1>
-                <div className="w-2/3">
-                    <p className="text-lg text-yellow-700 mb-4 mt-10 hover:text-yellow-600">
-                        WritePad is a Google Keep clone that helps you stay organized and productive. With WritePad, you can quickly jot down notes, make to-do lists, and set reminders for important tasks.
+                <div className="w-2/3 sm:w-1/2">
+                    <p className="text-base sm:text-lg text-yellow-700 mb-4 mt-10 hover:text-yellow-600">
+                        WritePad is a Google Keep clone that helps you stay organized and productive.
                     </p>
                 </div>
-                <div className="flex flex-wrap font-mono mt-24 ">
+
+                <div className="flex flex-col sm:flex-row font-mono mt-24">
                     {/* First video */}
-                    <div className="relative w-full md:w-1/2 flex items-center ">
-                        <video autoPlay loop className="ml-72 border border-yellow-700" width="320" height="180">
+                    <div className="relative w-full sm:w-1/2 flex items-center justify-center">
+                        <video autoPlay loop className="border border-yellow-700" width="320" height="180">
                             <source src="/writepad0.mov" type="video/mp4" />
                         </video>
                     </div>
-                    <div className="relative w-1/3 flex animate-pulse items-center font-mono text-yellow-500 pl-4">
-                        <div className="">
+                    <div className="relative w-full sm:w-1/2 flex items-center justify-center animate-pulse text-yellow-500 pl-4 mt-6 sm:mt-0">
+                        <div className="text-center">
                             <h2 className="text-lg font-bold mb-2">Add notes in style</h2>
                             <p className="text-sm">See how easy it is to add a note with a title and content to WritePad.</p>
                         </div>
                     </div>
 
                     {/* Second video */}
-                    <div className="relative w-full md:w-1/2 flex items-center">
-                        <video autoPlay loop className="ml-72 mt-10 border border-yellow-700" width="320" height="180">
+                    <div className="relative w-full sm:w-1/2 flex items-center justify-center mt-6 sm:mt-0">
+                        <video autoPlay loop className="border border-yellow-700" width="320" height="180">
                             <source src="/writepad1.mov" type="video/mp4" />
                         </video>
                     </div>
-                    <div className="relative w-1/3 flex items-center font-mono text-yellow-500 animate-pulse pl-4">
-                        <div>
+                    <div className="relative w-full sm:w-1/2 flex items-center justify-center animate-pulse text-yellow-500 pl-4 mt-6 sm:mt-0">
+                        <div className="text-center">
                             <h2 className="text-lg font-bold mb-2">Never lose a note again</h2>
                             <p className="text-sm">Watch how easy it is to delete a note and keep your workspace tidy.</p>
                         </div>
                     </div>
                 </div>
             </div>
-            {/* SVG */}
-
         </div>
     );
 }
